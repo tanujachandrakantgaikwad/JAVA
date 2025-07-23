d64eabdb70c6e6984f97e8cc1486e4517242c286
Tanuja Chandrakant Gaikwad <tanujagaikwad5585@gmail.com>
2025-07-23 19:55:51 +0530

    Add files via upload

---
+++ wrappperclass.java
@@ -0,0 +1,9 @@
+class Demo
+{
+    public static void main(String args[])
+      {
+         String t="33";
+         int a=Integer.parseInt(t);
+        System.out.println("Value="+a);
+      }
+}
---
+++ wrappperclass1.java
@@ -0,0 +1,9 @@
+class Demo
+{
+    public static void main(String args[])
+      {
+         String t="43.89";
+         float a=Float.parseFloat(t);
+        System.out.println("Value="+a);
+      }
+}
